8ceee802117712f4f24aefab21a020d4df346738
Emon Chowdhury <90343790+EmonChow@users.noreply.github.com>
2023-12-18 18:33:21 +0600

    final commit

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
